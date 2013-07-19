464d150f4d42e23e840d4db205428d17d7af45bf
aj0strow <alexander.ostrow@gmail.com>
2013-07-19 15:33:26 -0400

    added unique_name

--- lib/piza.rb
+++ lib/piza.rb
@@ -1,4 +1,11 @@
 require 'piza/version'
 
 module Piza
+  
+  @unique_name = '_piza_0_'
+  
+  def self.unique_name
+    @unique_name = @unique_name.next
+  end
+  
 end
--- spec/unit/piza_spec.rb
+++ spec/unit/piza_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
 describe Piza do
+  it 'should provide unique names' do
+    name = Piza.unique_name
+    expect(Piza.unique_name).not_to eq(name)
+  end
 end
